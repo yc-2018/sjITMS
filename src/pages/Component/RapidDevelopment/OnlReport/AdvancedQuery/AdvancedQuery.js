165f4ce40ed8411e47b6790d843cec96aae07198
LRC <liaorongchang@timeexpress.com.cn>
2022-02-24 10:39:02 +0800

    高级查询保存

--- src/pages/Component/RapidDevelopment/OnlReport/AdvancedQuery/AdvancedQuery.js
+++ src/pages/Component/RapidDevelopment/OnlReport/AdvancedQuery/AdvancedQuery.js
@@ -1,12 +1,21 @@
 import React, { Component, Fragment } from 'react';
-import { Empty, Button, Row, Col, Form, Select, Card, Modal } from 'antd';
+import { Empty, Button, Row, Col, Form, Select, Card, Modal, Input, Radio, Table, Tree } from 'antd';
 import ColsAdvanced from './ColsAdvanced';
 
 const layout = {
   wrapperCol: { span: 18 },
 };
+
 export default class AdvancedQuery extends Component {
-  state = { superQueryModalVisible: false };
+  state = {
+    superQueryModalVisible: false,
+    saveModalVisible: false,
+    saveName: '',
+    treeDatas: '',
+    passParams: ''
+  };
+
+
 
   //查询
   advanceQuery = () => {
@@ -19,13 +28,89 @@ export default class AdvancedQuery extends Component {
     this.formRef.onReset();
   };
 
+  //保存查询条件
+  onSave = () => {
+    this.setState({ saveModalVisible: true })
+  }
+
   //关闭窗口
   hideModal = () => {
     this.setState({ superQueryModalVisible: false });
   };
 
+  hideSaveModal = () => {
+    this.setState({ saveModalVisible: false });
+  }
+
+  onOk = () => {
+    const message = this.formRef.handleSubmit()
+    const { matchType, queryParams } = message
+    const queryParam = [];
+    queryParams.forEach((value, index) => {
+      queryParam.push({
+        key: index,
+        searchField: value.field,
+        searchCondition: value.rule,
+        defaultValue: value.val
+      })
+    })
+    let aa = new Array();
+    aa.push({
+      alias: this.state.saveName,
+      matchType: matchType,
+      queryParams: queryParam
+    })
+    this.setState({ treeDatas: [...this.state.treeDatas, aa] })
+    this.hideSaveModal()
+  }
+
+  changeSaveName = (value) => {
+    this.setState({ saveName: value })
+  }
+
+  onSelectTree = (selectedKeys) => {
+    const { treeDatas } = this.state
+    const data = []
+    treeDatas.forEach(datas => {
+      const treeData = datas.find(x => x.alias === selectedKeys[0])
+      if (treeData) {
+        data.push(treeData)
+      }
+    })
+    data.forEach(a => {
+      this.setState({ passParams: a.queryParams })
+    })
+  }
+
+  getRadio = () => {
+    const { treeDatas } = this.state
+    let radio = []
+    const treeData = []
+    if (treeDatas.length > 0) {
+      treeDatas.forEach(datas => {
+        datas.forEach(data => {
+          treeData.push({
+            title: data.alias,
+            key: data.alias,
+          })
+        })
+      })
+      radio.push(
+        <Tree
+          blockNode
+          style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
+          treeData={treeData}
+          onSelect={this.onSelectTree}
+        />
+      )
+    } else {
+      radio.push(<Empty description={<span style={{ color: '#aeb8c2' }}>没有保存任何查询</span>} />)
+    }
+    return radio;
+  }
+
   render() {
-    const { superQueryModalVisible } = this.state;
+    const { superQueryModalVisible, saveModalVisible, passParams } = this.state;
     const { searchFields, filterValue, reportCode } = this.props;
     return (
       <Fragment>
@@ -41,7 +126,7 @@ export default class AdvancedQuery extends Component {
             <Button key="1" onClick={this.onReset} style={{ float: 'left' }}>
               重置
             </Button>,
-            <Button key="2" style={{ float: 'left' }}>
+            <Button key="2" onClick={this.onSave} style={{ float: 'left' }}>
               保存查询条件
             </Button>,
             <Button key="3" onClick={this.hideModal}>
@@ -55,6 +140,7 @@ export default class AdvancedQuery extends Component {
           <Row>
             <Col span={18}>
               <ColsAdvanced
+                passParams={passParams}
                 formRefs={this.formRef}
                 layout={layout}
                 searchFields={searchFields}
@@ -67,10 +153,20 @@ export default class AdvancedQuery extends Component {
             </Col>
             <Col span={6}>
               <Card size="small" title="保存的查询">
-                <Empty description={<span style={{ color: '#aeb8c2' }}>没有保存任何查询</span>} />
+                {this.getRadio()}
               </Card>
             </Col>
           </Row>
+        </Modal>
+        <Modal
+          title="保存名称"
+          visible={saveModalVisible}
+          onCancel={this.hideSaveModal}
+          onOk={this.onOk}
+        >
+          <Form>
+            <Input placeholder="请输入保存的名称" onChange={(e) => this.changeSaveName(e.target.value)} />
+          </Form>
         </Modal>
       </Fragment>
     );
