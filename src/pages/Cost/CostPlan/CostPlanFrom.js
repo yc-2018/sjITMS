65ba1c1b0a02a932d814f27adb587856109ceffb
qiuhui <qiuhui@timeExpress.com>
2022-06-13 15:52:42 +0800

    路由修改

--- src/pages/Cost/CostPlan/CostPlanFrom.js
+++ src/pages/Cost/CostPlan/CostPlanFrom.js
@@ -5,7 +5,7 @@ import CostPlanDefCreate from './CostPlanDefCreate';
 import CostPlanFromSearch from './CostPlanFromSearch';
 import CostPlanView from './CostPlanView';
 import QuickForm from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickForm';
-
+import CostPlanIndex from './CostPlanIndex';
 @connect(({ quick, loading }) => ({
   quick,
   loading: loading.models.quick,
@@ -32,7 +32,7 @@ export default class CostPlanFrom extends QuickForm {
    */
   drawTab = e => {
     if (e.showPageNow == 'query') {
-      const component = <CostPlanFromSearch {...e.props} />;
+      const component = <CostPlanIndex {...e.props} />;
       e.component = component;
     }
     if (e.showPageNow == 'create') {
