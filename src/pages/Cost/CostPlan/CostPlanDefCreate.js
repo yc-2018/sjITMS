e35bc9b0a56e80f4805bdc6ab6c3464b7e7338a0
liaorongchang <liaorongchang@timeexpress.com.cn>
2022-08-08 09:32:04 +0800

    去b

--- src/pages/Cost/CostPlan/CostPlanDefCreate.js
+++ src/pages/Cost/CostPlan/CostPlanDefCreate.js
@@ -1,12 +1,11 @@
 import React, { useDebugValue } from 'react';
 import { connect } from 'dva';
 import { Form, Upload, Button, Icon, message, Layout } from 'antd';
-import { savePlan,addHistory} from '@/services/cost/Cost';
-import {makeFormData} from '@/pages/Cost/CostProject/CostProjectCreate'
+import { savePlan, addHistory } from '@/services/cost/Cost';
+import { makeFormData } from '@/pages/Cost/CostProject/CostProjectCreate';
 import QuickCreatePage from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickCreatePage';
-import CostPlanSearch from './CostPlanSearch'
-const {Footer, Content } = Layout;
-
+import CostPlanSearch from './CostPlanSearch';
+const { Footer, Content } = Layout;
 
 @connect(({ quick, loading }) => ({
   quick,
@@ -17,8 +16,8 @@ export default class CostPlanDefCreate extends QuickCreatePage {
   state = {
     ...this.state,
     isNotHd: true,
-    filelist: []
-  }
+    filelist: [],
+  };
   //继承QuickForm 重写drawTab方法 该方法用于重写跳转的界面
   /**
    * 
@@ -38,13 +37,13 @@ export default class CostPlanDefCreate extends QuickCreatePage {
       pathname: location.pathname,
    * }
    */
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     props.onRef && props.onRef(this);
   }
   /**
-     * 初始化表单数据
-     */
+   * 初始化表单数据
+   */
   initEntity = async () => {
     const { onlFormInfos } = this.state;
     //初始化entity
@@ -80,54 +79,53 @@ export default class CostPlanDefCreate extends QuickCreatePage {
       });
     }
   };
-  onSave = async (e) => {
+  onSave = async e => {
     e.preventDefault();
-     this.props.form.validateFields(async(err,values)=>{
-      if(!err){
-        const {list} = this.setting.state.data
-    var formDatas = new FormData();
-    makeFormData(this.entity.cost_plan[0], formDatas);
-    if(list!=undefined && list.length>0){
-      let paramList = []
-      list.map(item=>{
-        let param = {
-          uuid:item.uuid,
-          planUuid:item.PLAN_UUID,
-          projectUuid:item.UUID,
-          calcSort:item.CALC_SORT
+    this.props.form.validateFields(async (err, values) => {
+      if (!err) {
+        const { list } = this.setting.state.data;
+        var formDatas = new FormData();
+        makeFormData(this.entity.cost_plan[0], formDatas);
+        if (list != undefined && list.length > 0) {
+          let paramList = [];
+          list.map(item => {
+            let param = {
+              uuid: item.uuid,
+              planUuid: item.PLAN_UUID,
+              projectUuid: item.UUID,
+              calcSort: item.CALC_SORT,
+            };
+            paramList.push(param);
+          });
+          formDatas.append('costPlans', JSON.stringify(paramList));
         }
-        paramList.push(param);
 
-      })
-      formDatas.append("costPlans",JSON.stringify(paramList));
-    }
-     
-    this.state.filelist.forEach(element => {
-      if (!element.isSaved) {
-        formDatas.append('files', element.originFileObj);
-      }
-    });
-    let res = await savePlan(formDatas);
-    const success = res.data.success == true;
-    this.afterSave(success);
-    this.onSaved(success);
-    if (success) {
-      message.success('保存成功！');
-    }
-      }
-    });
-  }
-  addHistory = async()=>{
-    await addHistory(this.props?.params?.entityUuid).then(e=>{
-        if(e&& e.success){
-          message.success("添加历史版本成功");
+        this.state.filelist.forEach(element => {
+          if (!element.isSaved) {
+            formDatas.append('files', element.originFileObj);
+          }
+        });
+        let res = await savePlan(formDatas);
+        const success = res.data.success == true;
+        this.afterSave(success);
+        this.onSaved(success);
+        if (success) {
+          message.success('保存成功！');
         }
-    })
-  }
+      }
+    });
+  };
+  addHistory = async () => {
+    await addHistory(this.props?.params?.entityUuid).then(e => {
+      if (e && e.success) {
+        message.success('添加历史版本成功');
+      }
+    });
+  };
   formLoaded = () => {
     const { formItems } = this.state;
     formItems.cost_plan_ACCESSORY.component = this.uploadComponent;
-    this.setState({})
+    this.setState({});
   };
   uploadComponent = props => {
     return (
@@ -158,15 +156,42 @@ export default class CostPlanDefCreate extends QuickCreatePage {
     );
   };
   render() {
-    return <Layout style={{ backgroundColor: "white", height:'100%'}}>
-      <div style={{paddingTop:20}}>b
-        <Button type='primary' style={{float:'right',marginLeft:10,marginRight:10}}  onClick={this.addHistory}>添加历史记录</Button>
-        <Button type='primary' style={{float:'right',marginLeft:10,marginRight:10}} onClick={this.onSave}>保存</Button>
-        <Button style={{float:'right'}} onClick={() => { this.props.switchTab("query")} }>返回</Button>
-      </div>
-      <Content style={{marginLeft:'4.6%'}}>{this.drawForm()}</Content>
-      <Footer style={{ backgroundColor: "white"}}> <CostPlanSearch quickuuid ="cost_plan_item" PLAN_UUID = {this.props?.params?.entityUuid} 
-    onRef={c => (this.setting = c)}></CostPlanSearch></Footer>
-    </Layout>
+    return (
+      <Layout style={{ backgroundColor: 'white', height: '100%' }}>
+        <div style={{ paddingTop: 20 }}>
+          <Button
+            type="primary"
+            style={{ float: 'right', marginLeft: 10, marginRight: 10 }}
+            onClick={this.addHistory}
+          >
+            添加历史记录
+          </Button>
+          <Button
+            type="primary"
+            style={{ float: 'right', marginLeft: 10, marginRight: 10 }}
+            onClick={this.onSave}
+          >
+            保存
+          </Button>
+          <Button
+            style={{ float: 'right' }}
+            onClick={() => {
+              this.props.switchTab('query');
+            }}
+          >
+            返回
+          </Button>
+        </div>
+        <Content style={{ marginLeft: '4.6%' }}>{this.drawForm()}</Content>
+        <Footer style={{ backgroundColor: 'white' }}>
+          {' '}
+          <CostPlanSearch
+            quickuuid="cost_plan_item"
+            PLAN_UUID={this.props?.params?.entityUuid}
+            onRef={c => (this.setting = c)}
+          />
+        </Footer>
+      </Layout>
+    );
   }
 }
