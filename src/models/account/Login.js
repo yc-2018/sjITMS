af47d10e48a23c2a6c921497ed8019e70f341a9f
qiuhui <qiuhui@timeExpress.com>
2022-08-15 10:14:50 +0800

    登录后url修改

--- src/models/account/Login.js
+++ src/models/account/Login.js
@@ -51,7 +51,7 @@ export default {
             return;
           }
         }
-        yield put(routerRedux.replace('/bigData/zs/count'));
+        yield put(routerRedux.replace(redirect || '/bigData/zs/count'));
       } else {
         yield put({
           type: 'changeLoginStatus',
