0ff32d47b12af486921af9e9f49bb41d93d45c50
liaorongchang <liaorongchang@timeexpress.com.cn>
2022-09-09 10:07:13 +0800

    路桥费批量操作

--- src/pages/SJTms/DriverFee/DriverFeeSearchPage.js
+++ src/pages/SJTms/DriverFee/DriverFeeSearchPage.js
@@ -2,44 +2,70 @@
  * @Author: Liaorongchang
  * @Date: 2022-07-19 16:25:19
  * @LastEditors: Liaorongchang
- * @LastEditTime: 2022-09-08 15:56:16
+ * @LastEditTime: 2022-09-09 10:06:50
  * @version: 1.0
  */
 import { connect } from 'dva';
 import QuickFormSearchPage from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickFormSearchPage';
 import { Button, Popconfirm, message, Steps } from 'antd';
 import { approved } from '@/services/sjitms/TollFee';
+import BatchProcessConfirm from '../Dispatching/BatchProcessConfirm';
 
 @connect(({ quick, loading }) => ({
   quick,
   loading: loading.models.quick,
 }))
 export default class DriverFeeSearchPage extends QuickFormSearchPage {
-  approved = () => {
-    const { selectedRows } = this.state;
-    console.log('selectedRows', selectedRows);
-    if (selectedRows.length == 0) {
-      message.error('至少选一条数据');
-    }
-    selectedRows.forEach(async data => {
-      const result = await approved(data);
-    });
+  state = {
+    ...this.state,
+    showAuditPop: false,
+  };
+
+  approved = async data => {
+    return await approved(data);
   };
 
   //该方法用于写中间的功能按钮 多个按钮用<span>包裹
   drawToolsButton = () => {
+    const { showAuditPop, selectedRows } = this.state;
+
     return (
       <span>
         <Popconfirm
           title="你确定要审核所选中的内容吗?"
-          onConfirm={() => this.approved()}
-          okText="确定"
-          cancelText="取消"
+          visible={showAuditPop}
+          onVisibleChange={visible => {
+            if (!visible) this.setState({ showAuditPop: visible });
+          }}
+          onCancel={() => {
+            this.setState({ showAuditPop: false });
+          }}
+          onConfirm={() => {
+            this.setState({ showAuditPop: false });
+            this.approved(selectedRows[0]).then(response => {
+              if (response.success) {
+                message.success('审核成功！');
+                this.onSearch();
+              }
+            });
+          }}
         >
-          <Button>审批</Button>
+          <Button onClick={() => this.onBatchApproved()}>审批</Button>
         </Popconfirm>
+        <BatchProcessConfirm onRef={node => (this.batchProcessConfirmRef = node)} />
       </span>
     );
+  };
+
+  onBatchApproved = () => {
+    const { selectedRows } = this.state;
+    if (selectedRows.length == 0) {
+      message.warn('请选中一条数据！');
+      return;
+    }
+    selectedRows.length == 1
+      ? this.setState({ showAuditPop: true })
+      : this.batchProcessConfirmRef.show('审核', selectedRows, this.approved, this.onSearch);
   };
 
   drawcell = e => {
