05f06847c8686a3320f4728eebc2f5f622f557f4
“zhangze” <zhangze@timeexpress.com.cn>
2022-10-20 09:08:24 +0800

    解决报错

--- src/pages/SJTms/Dispatching/DispatchMapZ.js
+++ src/pages/SJTms/Dispatching/DispatchMapZ.js
@@ -9,7 +9,7 @@
 import React, { Component } from 'react';
 import { Divider, message } from 'antd';
 import { Map, Marker, Label, CustomOverlay, DrawingManager } from 'react-bmapgl';
-import B from 'BMapLib';
+// import B from 'BMapLib';
 // import { Map, Marker, Label, CustomOverlay } from '@uiw/react-baidu-map';
 
 import storeIcon from '@/assets/common/store.svg';
@@ -113,6 +113,52 @@ export default class DispatchMapZ extends Component {
       }
 
       this.map.addContextMenu(menu); // 给地图添加右键菜单
+
+      // var view = new mapvgl.View({
+      //   map: this.map,
+      // });
+
+      // var clusterLayer = new mapvgl.ClusterLayer({
+      //   minSize: 30, // 聚合点显示的最小直径
+      //   maxSize: 50, // 聚合点显示的最大直径
+      //   clusterRadius: 150, // 聚合范围半径
+      //   gradient: { 0: 'blue', 0.5: 'green', 1.0: 'red' }, // 聚合点颜色梯度
+      //   maxZoom: 15, // 聚合的最大级别，当地图放大级别高于此值将不再聚合
+      //   minZoom: 5, // 聚合的最小级别，当地图放大级别低于此值将不再聚合
+      //   // 是否显示文字
+      //   showText: true,
+      //   // 开始聚合的最少点数，点数多于此值才会被聚合
+      //   minPoints: 5,
+      //   // 设置文字样式
+      //   textOptions: {
+      //     fontSize: 12,
+      //     color: 'white',
+      //     // 格式化数字显示
+      //     format: function(count) {
+      //       return count >= 10000
+      //         ? Math.round(count / 1000) + 'k'
+      //         : count >= 1000
+      //           ? Math.round(count / 100) / 10 + 'k'
+      //           : count;
+      //     },
+      //   },
+      //   // 设置非聚合的点的icon
+      //   // iconOptions: {
+      //   //     width: 100 / 4,
+      //   //     height: 153 / 4,
+      //   //     icon: 'images/marker.png',
+      //   // },
+      //   enablePicked: true,
+      //   onClick(e) {
+      //     if (e.dataItem) {
+      //       // 可通过dataItem下面的children属性拿到被聚合的所有点
+      //       console.log(e.dataItem);
+      //     }
+      //   },
+      // });
+
+      // view.addLayer(clusterLayer);
+      // clusterLayer.setData(data);
     }, 1000);
 
     //缩放聚合 (引入js有问题)
