8043e9ace05cadd4fb20eecce190c3b8f1d03188
guankongjin@timeexpress.com.cn <guankongjin@timeexpress.com.cn>
2022-12-20 11:29:19 +0800

    客服工单修改

--- src/pages/SJTms/Customer/CustomerSearch.js
+++ src/pages/SJTms/Customer/CustomerSearch.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2022-12-19 17:48:10
  * @LastEditors: guankongjin
- * @LastEditTime: 2022-12-20 10:25:18
+ * @LastEditTime: 2022-12-20 11:28:40
  * @Description: file content
  * @FilePath: \iwms-web\src\pages\SJTms\Customer\CustomerSearch.js
  */
@@ -23,6 +23,12 @@ export default class CustomerSearch extends QuickFormSearchPage {
   drawToolsButton = () => {
     return (
       <>
+        <Button
+          onClick={() => this.handleDelete()}
+          hidden={!havePermission(this.state.authority + '.remove')}
+        >
+          删除
+        </Button>
         <Button
           onClick={() => this.handleRelease()}
           hidden={!havePermission(this.state.authority + '.release')}
@@ -77,13 +83,13 @@ export default class CustomerSearch extends QuickFormSearchPage {
     if (selectedRows.length == 1) {
       this.finished(selectedRows[0]).then(response => {
         if (response.success) {
-          message.success('发布成功！');
+          message.success('保存成功！');
           this.onSearch();
         }
       });
     } else {
       this.batchProcessConfirmRef.show(
-        '发布',
+        '完结',
         selectedRows.map(x => x.UUID),
         this.finished,
         this.onSearch
@@ -92,5 +98,16 @@ export default class CustomerSearch extends QuickFormSearchPage {
   };
   finished = async service => {
     return await finished(service.UUID);
+  };
+
+  //删除
+  handleDelete = () => {
+    const { selectedRows } = this.state;
+    const service = selectedRows.filter(x => x.STATUS != 'Saved').shift();
+    if (service) {
+      message.error('客服工单:' + service.BILLNUMBER + service.STATUS_CN + '状态，不能删除！');
+      return;
+    }
+    this.onBatchDelete();
   };
 }
