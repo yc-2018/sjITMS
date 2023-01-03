7f142f2eace7b1d727b90faac4f7ea71faf269bb
guankongjin@timeexpress.com.cn <guankongjin@timeexpress.com.cn>
2023-01-03 09:39:17 +0800

    客服工单优化

--- src/pages/SJTms/Customer/Customer.js
+++ src/pages/SJTms/Customer/Customer.js
@@ -2,14 +2,14 @@
  * @Author: Liaorongchang
  * @Date: 2022-04-15 16:05:33
  * @LastEditors: guankongjin
- * @LastEditTime: 2022-12-20 08:57:56
+ * @LastEditTime: 2022-12-30 17:16:37
  * @version: 1.0
  */
 import React from 'react';
 import { connect } from 'dva';
+import CustomerCreate from './CustomerCreate';
 import QuickForm from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickForm';
 import CustomerSearch from './CustomerSearch';
-import QuickCreatePage from '@/pages/Component/RapidDevelopment/OnlForm/QuickCreatePageDefault';
 import QuickViewPage from '@/pages/Component/RapidDevelopment/OnlForm/QuickViewPageDefault';
 
 @connect(({ quick, loading }) => ({
@@ -19,10 +19,10 @@ import QuickViewPage from '@/pages/Component/RapidDevelopment/OnlForm/QuickViewP
 export default class Customer extends QuickForm {
   drawTab = e => {
     if (e.showPageNow == 'create') {
-      const component = <QuickCreatePage {...e.props} />;
+      const component = <CustomerCreate {...e.props} />;
       e.component = component;
     } else if (e.showPageNow == 'update') {
-      const component = <QuickCreatePage {...e.props} />;
+      const component = <CustomerCreate {...e.props} />;
       e.component = component;
     } else if (e.showPageNow == 'view') {
       const component = <QuickViewPage {...e.props} />;
