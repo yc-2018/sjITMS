8aa5ecbce47b8a01c92f0ee3ac89d2461347fd67
guankongjin@timeexpress.com.cn <guankongjin@timeexpress.com.cn>
2023-01-12 14:40:35 +0800

    添加必填设置

--- src/pages/SJTms/Customer/CustomerCreate.js
+++ src/pages/SJTms/Customer/CustomerCreate.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2023-01-03 11:44:10
  * @LastEditors: guankongjin
- * @LastEditTime: 2023-01-04 14:44:31
+ * @LastEditTime: 2023-01-12 14:36:30
  * @Description: file content
  * @FilePath: \iwms-web\src\pages\SJTms\Customer\CustomerCreate.js
  */
@@ -29,6 +29,26 @@ export default class CustomerCreate extends QuickCreatePage {
   exHandleChange = columnEvent => {
     const mainName = 'sj_itms_customer_service';
     const { fieldName, valueEvent } = columnEvent;
+    if (fieldName == 'RESPONSIBILITYGROUP' && valueEvent) {
+      const { formItems } = this.state;
+      let rules = formItems['sj_itms_customer_service_RESPONSIBILITYCODE'].rules;
+      rules.forEach(rule => {
+        if (rule.hasOwnProperty('required')) {
+          rule.required = valueEvent.record.DESCRIPTION === '1';
+        }
+      });
+      this.setState({ formItems });
+    }
+    if (fieldName == 'SPECIFICTYPE' && valueEvent) {
+      const { formItems } = this.state;
+      let rules = formItems['sj_itms_customer_service_RESULTTAG'].rules;
+      rules.forEach(rule => {
+        if (rule.hasOwnProperty('required')) {
+          rule.required = valueEvent.record.REQUIRED === 1;
+        }
+      });
+      this.setState({ formItems });
+    }
     if (fieldName == 'DISPOSEDEPT' && valueEvent) {
       this.entity[mainName][0]['DISPOSEDEPTNAME'] = valueEvent.record.NAME;
       this.entity[mainName][0]['DISPOSECODE'] = valueEvent.record.MANAGERCODE?.split(',')[0];
