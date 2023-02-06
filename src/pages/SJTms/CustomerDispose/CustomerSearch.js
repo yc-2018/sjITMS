446a0c9136ecb9f990a9eac76d4ad4babbc42d3c
guankongjin <guankongjin@timeexpress.com.cn>
2023-02-06 17:59:55 +0800

    工单处理修改

--- src/pages/SJTms/CustomerDispose/CustomerSearch.js
+++ src/pages/SJTms/CustomerDispose/CustomerSearch.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2023-01-07 16:10:19
  * @LastEditors: guankongjin
- * @LastEditTime: 2023-01-11 16:37:34
+ * @LastEditTime: 2023-02-06 17:59:29
  * @Description: file content
  * @FilePath: \iwms-web\src\pages\SJTms\CustomerDispose\CustomerSearch.js
  */
@@ -10,6 +10,7 @@ import React from 'react';
 import { connect } from 'dva';
 import QuickFormSearchPage from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickFormSearchPage';
 import DisposePage from './DisposePage';
+import { loginUser } from '@/utils/LoginContext';
 
 @connect(({ quick, loading }) => ({
   quick,
@@ -26,7 +27,10 @@ export default class CustomerSearch extends QuickFormSearchPage {
     }
   };
   exSearchFilter = () => {
-    return [{ field: 'STATUS', type: 'VarChar', rule: 'ne', val: 'Saved' }];
+    return [
+      { field: 'STATUS', type: 'VarChar', rule: 'ne', val: 'Saved' },
+      { field: 'DISPOSECODE', type: 'VarChar', rule: 'eq', val: loginUser().code },
+    ];
   };
   drawToolbarPanel = () => {};
 
