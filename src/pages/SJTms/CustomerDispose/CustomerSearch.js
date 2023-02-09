1e7b598e97ebc3a324f8ff1e59cd8a982608a4be
guankongjin <guankongjin@timeexpress.com.cn>
2023-02-09 15:45:16 +0800

    客服工单处理查询修改

--- src/pages/SJTms/CustomerDispose/CustomerSearch.js
+++ src/pages/SJTms/CustomerDispose/CustomerSearch.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2023-01-07 16:10:19
  * @LastEditors: guankongjin
- * @LastEditTime: 2023-02-06 17:59:29
+ * @LastEditTime: 2023-02-09 15:43:51
  * @Description: file content
  * @FilePath: \iwms-web\src\pages\SJTms\CustomerDispose\CustomerSearch.js
  */
@@ -11,6 +11,7 @@ import { connect } from 'dva';
 import QuickFormSearchPage from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickFormSearchPage';
 import DisposePage from './DisposePage';
 import { loginUser } from '@/utils/LoginContext';
+import { havePermission } from '@/utils/authority';
 
 @connect(({ quick, loading }) => ({
   quick,
@@ -27,10 +28,11 @@ export default class CustomerSearch extends QuickFormSearchPage {
     }
   };
   exSearchFilter = () => {
-    return [
-      { field: 'STATUS', type: 'VarChar', rule: 'ne', val: 'Saved' },
-      { field: 'DISPOSECODE', type: 'VarChar', rule: 'eq', val: loginUser().code },
-    ];
+    let param = [{ field: 'STATUS', type: 'VarChar', rule: 'ne', val: 'Saved' }];
+    if (!havePermission('sjtms.customer.service.view')) {
+      param.push({ field: 'DISPOSECODE', type: 'VarChar', rule: 'eq', val: loginUser().code });
+    }
+    return param;
   };
   drawToolbarPanel = () => {};
 
