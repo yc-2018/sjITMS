1027ebb6a20c0d2c3cc04912443d4c7abe81e29d
liaorongchang <liaorongchang@timeexpress.com.cn>
2023-03-21 16:46:10 +0800

    排车单客服默认调度中心

--- src/pages/SJTms/ScheduleCustomer/ScheduleCustomerSearchPage.js
+++ src/pages/SJTms/ScheduleCustomer/ScheduleCustomerSearchPage.js
@@ -2,42 +2,78 @@
  * @Author: Liaorongchang
  * @Date: 2022-07-19 16:25:19
  * @LastEditors: Liaorongchang
- * @LastEditTime: 2023-03-07 14:24:52
+ * @LastEditTime: 2023-03-21 16:38:43
  * @version: 1.0
  */
 import { connect } from 'dva';
 import QuickFormSearchPage from '@/pages/Component/RapidDevelopment/OnlForm/Base/QuickFormSearchPage';
-import { havePermission } from '@/utils/authority';
-import { Button } from 'antd';
-import { getTableColumns } from '@/utils/LoginContext';
-import ExportJsonExcel from 'js-export-excel';
+import { loginOrg } from '@/utils/LoginContext';
+import { clearPageFilter, getActiveKey, getPageFilter } from '@/utils/LoginContext';
 
 @connect(({ quick, loading }) => ({
   quick,
   loading: loading.models.quick,
 }))
 export default class ScheduleReportSearchPage extends QuickFormSearchPage {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.state,
-      title: '',
-      data: [],
-      suspendLoading: false,
-      columns: [],
-      searchFields: [],
-      advancedFields: [],
-      reportCode: props.quickuuid,
-      isOrgQuery: [],
-      key: props.quickuuid + 'quick.search.customber', //用于缓存用户配置数据
-      defaultSort: '',
-      formConfig: {},
-      colTotal: [],
-      queryConfigColumns: [],
-      tableName: '',
-      authority: props.route?.authority ? props.route.authority[0] : null,
-    };
+  componentDidMount() {
+    this.queryCoulumns();
+    this.getCreateConfig();
   }
+
+  defaultSearch = () => {
+    this.setState({pageFilters:""})
+    //默认查询
+    let ex = this.state.queryConfigColumns.filter(item => {
+      return item.searchDefVal != null && item.searchDefVal != '';
+    });
+    let defaultSearch = [];
+    let exSearchFilter;
+    for (const item of ex) {
+      if (item.fieldType == 'Date') {
+        let days = parseInt(item.searchDefVal);
+        if (days != days) days = 0;
+        let endDate = moment(new Date()).format('YYYY-MM-DD');
+        let startDate = moment(new Date())
+          .add(-item.searchDefVal, 'days')
+          .format('YYYY-MM-DD');
+        exSearchFilter = {
+          field: item.fieldName,
+          type: item.fieldType,
+          rule: item.searchCondition,
+          val: `${startDate}||${endDate}`,
+        };
+      } else if (item.fieldType == 'DateTime') {
+        let days = parseInt(item.searchDefVal);
+        if (days != days) days = 0;
+        let endDate = moment(new Date()).format('YYYY-MM-DD 23:59:59');
+        let startDate = moment(new Date())
+          .add(-item.searchDefVal, 'days')
+          .format('YYYY-MM-DD 00:00:00');
+        exSearchFilter = {
+          field: item.fieldName,
+          type: item.fieldType,
+          rule: item.searchCondition,
+          val: `${startDate}||${endDate}`,
+        };
+      } else {
+        exSearchFilter = {
+          field: item.fieldName,
+          type: item.fieldType,
+          rule: item.searchCondition,
+          val: item.searchDefVal,
+        };
+      }
+      defaultSearch.push(exSearchFilter);
+    }
+    //暂时通过这种方式赋予默认值
+    defaultSearch.push({
+      field: 'DISPATCHCENTERUUID',
+      type: 'VARCHAR',
+      rule: 'eq',
+      val: loginOrg().uuid,
+    });
+    return defaultSearch;
+  };
 
   //查询数据
   getData = pageFilters => {
@@ -62,8 +98,4 @@ export default class ScheduleReportSearchPage extends QuickFormSearchPage {
       },
     });
   };
-
-  
-
- 
 }
