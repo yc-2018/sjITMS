3e22ddf67ac1ec424ce093000b77029279199d71
liaorongchang <liaorongchang@timeexpress.com.cn>
2023-03-31 14:46:47 +0800

    粤东司机出入厂刷卡未超3小时提醒

--- src/pages/SJTms/Schedule/ScheduleSearchPage.js
+++ src/pages/SJTms/Schedule/ScheduleSearchPage.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2022-06-29 16:26:59
  * @LastEditors: Liaorongchang
- * @LastEditTime: 2023-03-30 10:22:49
+ * @LastEditTime: 2023-03-30 17:53:26
  * @Description: 排车单列表
  * @FilePath: \iwms-web\src\pages\SJTms\Schedule\ScheduleSearchPage.js
  */
@@ -582,7 +582,7 @@ export default class ScheduleSearchPage extends QuickFormSearchPage {
         >
           <Button
             onClick={() => this.onBatchRefresh()}
-            // hidden={!havePermission(this.state.authority + '.rollBack')}
+            hidden={!havePermission(this.state.authority + '.refresh')}
           >
             ETC资料刷新
           </Button>
