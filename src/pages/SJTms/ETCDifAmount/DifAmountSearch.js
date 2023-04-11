59c87796e098dc0003f22f790ad43c5d1388f75b
liaorongchang <liaorongchang@timeexpress.com.cn>
2023-04-11 09:58:01 +0800

    1

--- src/pages/SJTms/ETCDifAmount/DifAmountSearch.js
+++ src/pages/SJTms/ETCDifAmount/DifAmountSearch.js
@@ -2,7 +2,7 @@
  * @Author: Liaorongchang
  * @Date: 2022-06-30 09:27:20
  * @LastEditors: Liaorongchang
- * @LastEditTime: 2023-04-06 09:39:57
+ * @LastEditTime: 2023-04-11 09:23:59
  * @version: 1.0
  */
 import { connect } from 'dva';
@@ -23,25 +23,6 @@ export default class DifAmountSearch extends QuickFormSearchPage {
   //需要操作列的显示 将noActionCol设置为false
   state = { ...this.state, noActionCol: false, isShow: false, isLoading: false };
 
-  // handleUploading = info => {
-  //   if (info.file.status == 'uploading') {
-  //     this.setState({ isLoading: true });
-  //   }
-  //   if (info.file.status === 'done') {
-  //     this.setState({ isLoading: false });
-  //     message.success(`上传成功`);
-  //     this.refreshTable();
-  //   } else if (info.file.status === 'error') {
-  //     this.setState({ isLoading: false });
-  //     Modal.confirm({
-  //       title: '导入失败',
-  //       content: <div style={{ whiteSpace: 'pre-line' }}>{info.file.response.message}</div>,
-  //       okText: '确认',
-  //       cancelText: '取消',
-  //     });
-  //   }
-  // };
-
   upLoading = () => {
     this.props.switchTab('import');
   };
@@ -51,26 +32,13 @@ export default class DifAmountSearch extends QuickFormSearchPage {
     const { isLoading } = this.state;
     return (
       <span>
-        {/* <Upload
-          name="file"
-          action={
-            configs[API_ENV].API_SERVER +
-            `/itms-schedule/itms-schedule/sj/etc/import?companyUuid=${loginCompany().uuid}&dcUuid=${
-              loginOrg().uuid
-            }`
-          }
-          showUploadList={false}
-          headers={{ iwmsJwt: loginKey() }}
-          accept=".xlsx"
-          onChange={info => {
-            this.handleUploading(info);
-          }}
-        > */}
-        <Button onClick={this.upLoading.bind()}>
+        <Button
+          onClick={this.upLoading.bind()}
+          hidden={!havePermission(this.state.authority + '.import')}
+        >
           <Icon type="upload" loading={isLoading} />
           导入
         </Button>
-        {/* </Upload> */}
       </span>
     );
   };
