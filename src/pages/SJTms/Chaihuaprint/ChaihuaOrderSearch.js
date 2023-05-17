6f92f6353686f18078b8a646d09a6fa85df93d4c
qiuhui <qiuhui@timeExpress.com>
2023-05-17 21:05:32 +0800

    456

--- src/pages/SJTms/Chaihuaprint/ChaihuaOrderSearch.js
+++ src/pages/SJTms/Chaihuaprint/ChaihuaOrderSearch.js
@@ -256,7 +256,7 @@ export default class TranOrderSearch extends QuickFormSearchPage {
     const printPages = document.getElementById('printPagewes').childNodes;
     printPages.forEach(page => {
       LODOP.NewPageA();
-      LODOP.ADD_PRINT_TABLE('2%', '2%', '96%', '96%', page.innerHTML);
+      LODOP.ADD_PRINT_TABLE('2%', '0.5%', '96%', '96%', page.innerHTML);
       //LODOP.ADD_PRINT_HTM('2%', '2%', '96%', '96%', page.innerHTML);
 
     });
@@ -337,7 +337,7 @@ export default class TranOrderSearch extends QuickFormSearchPage {
       return (
         <div>
           <table
-            style={{ width: '95%', borderCollapse: 'collapse',fontFamily: "微软雅黑", fontWeight: 'normal',fontSize: 13, border: 0 }}
+            style={{ width: '94%', borderCollapse: 'collapse',fontFamily: "微软雅黑", fontWeight: 'normal',fontSize: 13, border: 0 }}
             border={1}
             cellPadding={0}
             cellSpacing={0}
@@ -390,16 +390,16 @@ export default class TranOrderSearch extends QuickFormSearchPage {
 
               <tr style={{ height: 25 }}>
                 <th width={30}>序号</th>
-                <th width={70} style={{fontSize:11}}>商品代码</th>
-                <th width={60} style={{fontSize:11}}>国际条码</th>
+                <th width={60}>商品代码</th>
+                <th width={40}>国际条码</th>
                 <th width={110}>品名</th>
-                <th width={40} style={{fontSize:11}}>批次</th>
+                <th width={40}>批次</th>
                 <th width={30}>单位</th>
                 <th width={40}>数量</th>
                 <th width={40}>包装</th>
                 <th width={40}>件数</th>
                 <th width={40}>销售价</th>
-                <th width={40}>销售金额</th>
+                <th width={50}>销售金额</th>
                 <th width={40}>折扣金额</th>
               </tr>
             </thead>
@@ -411,9 +411,9 @@ export default class TranOrderSearch extends QuickFormSearchPage {
                       return (
                         <tr style={{ textAlign: 'center', height: 20 }}>
                           <td width={30}>{index + 1}</td>
-                          <td width={70} style={{textAlign: 'left',fontSize:11}}
+                          <td width={60} style={{textAlign: 'left'}}
                           >{item.ARTICLECODE}</td>
-                          <td style={{textAlign: 'left',fontSize:11}} width={60}>{item.BARCODE}</td>
+                          <td style={{textAlign: 'left',fontSize:11}} width={40}>{item.BARCODE}</td>
                           <td style={{textAlign: 'left'}} width={110}>{item.ARTICLENAME}</td>
                           <td width={40}>{item.BATCHNUM}</td>
                           <td width={30}>{item.MUNIT}</td>
@@ -423,7 +423,7 @@ export default class TranOrderSearch extends QuickFormSearchPage {
                           <td width={40}>{item.PAQ}</td>
                           <td width={40} >{item.REALQTYSTR}</td>
                           <td width={40}>{item.PRICE}</td>
-                          <td width={40}>{item.PRICE1}</td>
+                          <td width={50}>{item.PRICE1}</td>
                           <td width={40}>{ }</td>
                         </tr>
                       );
@@ -441,10 +441,10 @@ export default class TranOrderSearch extends QuickFormSearchPage {
                 heands.map((item, index) => {
                   return (
                     <tr style={{ textAlign: 'center', height: 20 }}>
-                      <td width={80} colSpan={3}>{item.SOURCEBILLNUMBER}</td>
-                      <td width={80} colSpan={5}>{item.BILLNUMBER}</td>
-                      <td width={80} colSpan={2}>{item.BNUMBERS}</td>
-                      <td width={80} colSpan={2}>{item.TOTALREALAMOUNT}</td>
+                      <td  colSpan={3}>{item.SOURCEBILLNUMBER}</td>
+                      <td  colSpan={5}>{item.BILLNUMBER}</td>
+                      <td  colSpan={2}>{item.BNUMBERS}</td>
+                      <td colSpan={2}>{item.TOTALREALAMOUNT}</td>
                     </tr>
                   );
                 })
@@ -456,8 +456,8 @@ export default class TranOrderSearch extends QuickFormSearchPage {
                 heandsdd.map((item, index) => {
                   return (
                     <tr style={{ textAlign: 'center', height: 20 }}>
-                      <td width={80} colSpan={10}>{item.SELLDISCOUNTORDERNO}</td>
-                      <td width={80} colSpan={2}>{item.CASHTICKETMONEY}</td>
+                      <td  colSpan={10}>{item.SELLDISCOUNTORDERNO}</td>
+                      <td  colSpan={2}>{item.CASHTICKETMONEY}</td>
                     </tr>
                   );
                 })
