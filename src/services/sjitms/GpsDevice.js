3f6b8af7093269e79eccb6c6d8c092ae4d177ea4
guankongjin <guankongjin@timeexpress.com.cn>
2023-10-10 12:06:57 +0800

    G7设备管理功能优化

---
+++ src/services/sjitms/GpsDevice.js
@@ -0,0 +1,22 @@
+import request from '@/utils/request';
+
+export async function shift(gpsNos, truckId, truckNo) {
+  return request(`/itms-schedule/itms-schedule/g7/deivce/shift?gpsNos=${gpsNos}&truckId=${truckId}&truckNo=${truckNo}`,
+    {
+      method: 'POST'
+    });
+}
+
+export async function split(gpsNos) {
+  return request(`/itms-schedule/itms-schedule/g7/deivce/split?gpsNos=${gpsNos}`,
+    {
+      method: 'POST'
+    });
+}
+
+export async function saveServiceDate(gpsNos, type, serviceDate) {
+  return request(`/itms-schedule/itms-schedule/g7/deivce/saveServiceDate?gpsNos=${gpsNos}&type=${type}&serviceDate=${serviceDate}`,
+    {
+      method: 'POST'
+    });
+}
