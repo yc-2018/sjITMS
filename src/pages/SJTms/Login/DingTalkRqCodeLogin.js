85a241610c83de82a03c488d667b1420bb677648
“zhangze” <zhangze@timeexpress.com.cn>
2024-01-05 16:18:06 +0800

    新增登录区分BMS TMS

--- src/pages/SJTms/Login/DingTalkRqCodeLogin.js
+++ src/pages/SJTms/Login/DingTalkRqCodeLogin.js
@@ -6,56 +6,60 @@
  * @Description: 钉钉登录
  * @FilePath: \iwms-qq\src\pages\Magpie\Login\DingTalkRqCodeLogin.js
  */
-import React from "react";
+import React from 'react';
 import Script from 'react-load-script';
 import { message } from 'antd';
 import dingtalk from '@/assets/login/dingtalk.svg';
 import styles from './Login.less';
 
-export default function DingTalkRqCodeLogin(dispatch) {
-    function handleScriptLoad() {
-        const appid = "dingcklpj9ydkhrkwuxw";
-        const redirect_uri = encodeURIComponent(`${window.origin}`);
-        window.DTFrameLogin(
-            {
-                id: 'login_container',
-                width: 280,
-                height: 280
-            },
-            {
-                redirect_uri: redirect_uri,
-                client_id: appid,
-                scope: 'openid',
-                response_type: 'code',
-                prompt: 'consent',
-                state: 86
-            },
-            async (loginResult) => {
-                const { authCode } = loginResult;
-                // 登录
-               await dispatch({ type: 'login/dingLogin', payload: { authCode } });
-            },
-            (errorMsg) => {
-                // 这里一般需要展示登录失败的具体原因
-                message.error(`Login Error: ${errorMsg}`);
-            },
-        );
-    }
-    function login() {
-        const appid = "dingcklpj9ydkhrkwuxw";
-        const redirect_uri = encodeURIComponent(`${window.location.origin}/user/dinglogin`);
-        window.location.href = `https://login.dingtalk.com/oauth2/auth?redirect_uri=${redirect_uri}&response_type=code&client_id=${appid}&scope=openid&state=86&prompt=consent`;
-    }
+export default function DingTalkRqCodeLogin(dispatch, orgType) {
+  function handleScriptLoad() {
+    const appid = 'dingcklpj9ydkhrkwuxw';
+    const redirect_uri = encodeURIComponent(`${window.origin}`);
+    window.DTFrameLogin(
+      {
+        id: 'login_container',
+        width: 280,
+        height: 280,
+      },
+      {
+        redirect_uri: redirect_uri,
+        client_id: appid,
+        scope: 'openid',
+        response_type: 'code',
+        prompt: 'consent',
+        state: 86,
+      },
+      async loginResult => {
+        const { authCode } = loginResult;
+        // 登录
+        await dispatch({ type: 'login/dingLogin', payload: { authCode, orgType } });
+      },
+      errorMsg => {
+        // 这里一般需要展示登录失败的具体原因
+        message.error(`Login Error: ${errorMsg}`);
+      }
+    );
+  }
+  function login() {
+    const appid = 'dingcklpj9ydkhrkwuxw';
+    const redirect_uri = encodeURIComponent(
+      `${window.location.origin}/user/dinglogin?orgType=${orgType}`
+    );
+    window.location.href = `https://login.dingtalk.com/oauth2/auth?redirect_uri=${redirect_uri}&response_type=code&client_id=${appid}&scope=openid&state=86&prompt=consent`;
+  }
 
-    return <div className={styles.dingtalkLoginCell}>
-        <Script
-            url="https://g.alicdn.com/dingding/h5-dingtalk-login/0.21.0/ddlogin.js"
-            onLoad={handleScriptLoad}
-        />
-        <div id='login_container' className={styles.dingtalkLoginContainer}></div>
-        <a href="#" className={styles.dingtalkAccountLoginContainer} onClick={login}>
-            <img src={dingtalk} className={styles.dingtalkLoginLink} />
-            <span> 钉钉账号登录</span>
-        </a>
-    </div>;
+  return (
+    <div className={styles.dingtalkLoginCell}>
+      <Script
+        url="https://g.alicdn.com/dingding/h5-dingtalk-login/0.21.0/ddlogin.js"
+        onLoad={handleScriptLoad}
+      />
+      <div id="login_container" className={styles.dingtalkLoginContainer} />
+      <a href="#" className={styles.dingtalkAccountLoginContainer} onClick={login}>
+        <img src={dingtalk} className={styles.dingtalkLoginLink} />
+        <span> 钉钉账号登录</span>
+      </a>
+    </div>
+  );
 }
