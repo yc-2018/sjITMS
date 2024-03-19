1cd6fccabab4948933a950aa1ff14623339ddeb5
guankongjin <guankongjin@timeexpress.com.cn>
2024-03-19 10:52:17 +0800

    司机服务打印模板优化

--- src/pages/SJTms/DriverCustomerDispose/Print.js
+++ src/pages/SJTms/DriverCustomerDispose/Print.js
@@ -2,7 +2,7 @@
  * @Author: guankongjin
  * @Date: 2024-03-08 14:02:18
  * @LastEditors: guankongjin
- * @LastEditTime: 2024-03-08 16:11:04
+ * @LastEditTime: 2024-03-19 10:52:11
  * @Description: 打印拣货记录
  * @FilePath: \iwms-web\src\pages\SJTms\DriverCustomerDispose\Print.js
  */
@@ -31,16 +31,16 @@ export const drawPrintPage = (detail, records) => {
               <div style={{ textAlign: 'left', fontWeight: 'normal', height: 20 }}>
                 <div style={{ float: 'left', width: '30%' }}>制单人: {loginUser().name}</div>
                 <div style={{ float: 'left', width: '32%' }}>打印时间: {convertDateToTime(new Date())}</div>
-                <div style={{ float: 'left', width: '32%' }}>申请货品:{detail.articlecode}    申请数量:{detail.qty}</div>
+                <div style={{ float: 'left', width: '32%' }}>申请货品:{detail.articlecode}</div>
               </div>
             </th>
           </tr>
-          <tr style={{ height: 25, borderColor: "#011627" }}>
+          <tr style={{ textAlign: 'center', height: 25, borderColor: "#011627" }}>
             <th width={100}>周转箱号</th>
             <th width={50}>捡货位</th>
             <th width={50}>货品代码</th>
             <th width={50}>拣货门店</th>
-            <th width={50}>计划数量</th>
+            <th width={50}>差异数量</th>
             <th width={50}>实际数量</th>
             <th width={120}>确认时间</th>
             <th width={200}>货品名称</th>
@@ -52,20 +52,19 @@ export const drawPrintPage = (detail, records) => {
               return (
                 <tr
                   style={{
-                    textAlign: 'center',
                     height: 20,
                     borderColor: "#011627",
                     fontSize: detail.articlecode == item.sku ? 14 : 12,
                     fontWeight: detail.articlecode == item.sku ? "bold" : "normal"
                   }}
                 >
-                  <td width={100}>{item.cartonno}</td>
-                  <td width={50}>{item.fmlocation}</td>
-                  <td width={50}>{item.sku}</td>
-                  <td width={50}>{item.consigneeid}</td>
-                  <td width={50}>{item.fmqtyEach}</td>
-                  <td width={50}>{item.qtyordered}</td>
-                  <td width={120}>{item.edittime}</td>
+                  <td width={90}>{item.cartonno}</td>
+                  <td width={50} style={{textAlign: 'center'}}>{item.fmlocation}</td>
+                  <td width={50} style={{textAlign: 'center'}}>{item.sku}</td>
+                  <td width={50} style={{textAlign: 'center'}}>{item.consigneeid}</td>
+                  <td width={50} style={{textAlign: 'center'}}>{detail.articlecode == item.sku ? detail.qty : ""}</td>
+                  <td width={50} style={{textAlign: 'center'}}>{item.qtyordered}</td>
+                  <td width={130} style={{textAlign: 'center'}}>{item.edittime}</td>
                   <td width={200}>{item.descrC}</td>
                 </tr>
               );
@@ -77,9 +76,7 @@ export const drawPrintPage = (detail, records) => {
         <tfoot>
           <tr style={{ height: 25, borderColor: "#011627" }}>
             <td colspan={4}>合计:</td>
-            <td style={centerStyle}>
-              {records ? (Math.round(sumBy(records, x => x.fmqtyEach) * 100) / 100) : 0}
-            </td>
+            <td style={centerStyle}>{detail.qty}</td>
             <td style={centerStyle}>
               {records ? (Math.round(sumBy(records, x => x.qtyordered) * 100) / 100) : 0}
             </td>
