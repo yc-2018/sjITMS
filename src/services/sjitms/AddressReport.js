1454f4aed4e8e7c79d0ff8a60f0819983046ffaf
ChenGuangLong <chenguanglong@timeexpress.com.cn>
2024-05-27 14:39:01 +0800

    【审核门店经纬度组件】增加接口：｛获取门店图片URL地址，通过审核，作废｝

--- src/services/sjitms/AddressReport.js
+++ src/services/sjitms/AddressReport.js
@@ -6,7 +6,43 @@
  * @version: 1.0
  */
 import request from '@/utils/request';
-import { loginCompany, loginOrg } from '@/utils/LoginContext';
+
+/**
+ * 获取门店图片URL地址
+ * @param {String} uuid 审核单uuid
+ * @author ChenGuangLong
+ * @since 2024/5/27 10:10
+*/
+export async function getStoreImgList(uuid) {
+  return request(`/itms-schedule/itms-schedule/addressReport/getStoreImg?uuid=${uuid}`, {
+    method: 'POST'
+  });
+}
+
+/**
+ * 通过审核
+ * @param {String} uuid 审核单uuid
+ * @author ChenGuangLong
+ * @since 2024/5/27 10:42
+*/
+export async function audit(uuid) {
+  return request(`/itms-schedule/itms-schedule/addressReport/audit?uuid=${uuid}`, {
+    method: 'POST'
+  });
+}
+
+/**
+ * 作废
+ * @param {String} uuid 审核单uuid
+ * @author ChenGuangLong
+ * @since 2024/5/27 10:41
+*/
+export async function voided(uuid) {
+  return request(`/itms-schedule/itms-schedule/addressReport/voided?uuid=${uuid}`, {
+    method: 'POST'
+  });
+}
+
 
 
 export async function audits(params) {
