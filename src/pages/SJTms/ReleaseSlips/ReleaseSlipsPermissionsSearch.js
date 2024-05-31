2e30d6d26925ee258343a94c85bd984b0051290a
ChenGuangLong <chenguanglong@timeexpress.com.cn>
2024-05-31 15:39:35 +0800

    放行条权限界面

--- src/pages/SJTms/ReleaseSlips/ReleaseSlipsPermissionsSearch.js
+++ src/pages/SJTms/ReleaseSlips/ReleaseSlipsPermissionsSearch.js
@@ -4,7 +4,7 @@ import { connect } from 'dva'
 import React from 'react'
 import CreatePageModal from '@/pages/Component/RapidDevelopment/OnlForm/QuickCreatePageModal'
 import { dynamicDelete } from '@/services/quick/Quick'
-import { loginCompany, loginOrg } from '@/utils/LoginContext'
+import { loginOrg } from '@/utils/LoginContext'
 
 @connect(({ quick, loading }) => ({
   quick,
@@ -16,57 +16,56 @@ export default class ReleaseSlipsPermissionsSearch extends QuickFormSearchPage {
     noTable: true,      // 框架的表格不显示
   }
 
-/**
- * 查询数据(重写为了把每页改成99条数据 和 分调度中心）
- * @author ChenGuangLong
- * @since 2024/5/31 14:16
-*/
+  /**
+   * 查询数据(重写为了把每页改成99条数据 和 分调度中心）
+   * @author ChenGuangLong
+   * @since 2024/5/31 14:16
+   */
   getData = pageFilters => {
     pageFilters.pageSize = 99     // 一页
-    pageFilters.superQuery.queryParams[0]= {
-      field: "dispatchcenterUuid",
-      type: "VarChar",
-      rule: "eq",
+    pageFilters.superQuery.queryParams[0] = {
+      field: 'dispatchcenterUuid',
+      type: 'VarChar',
+      rule: 'eq',
       val: loginOrg().uuid        // 分调度中心
     }
 
-    const { dispatch } = this.props;
+    const { dispatch } = this.props
     dispatch({
       type: 'quick/queryData',
       payload: pageFilters,
       callback: response => {
-        if (response.data) this.initData(response.data);
+        if (response.data) this.initData(response.data)
       },
-    });
-  };
+    })
+  }
 
   /**
    * 该方法会覆盖所有的上层按钮
    * @author ChenGuangLong
    * @since 2024/5/31 10:35
-  */
+   */
   drawActionButton = () => <Button onClick={() => this.createPageModalRef.show()}>新增放行权限人</Button>
 
   /**
    * 绘制搜索
    * @author ChenGuangLong
    * @since 2024/5/31 10:34
-  */
+   */
   drawSearchPanel = () => {}
-
 
   /**
    * 该方法会覆盖所有的中间功能按钮
    * @author ChenGuangLong
    * @since 2024/5/31 10:35
-  */
+   */
   drawToolbarPanel = () => {}
 
-/**
- * 画出主要界面
- * @author ChenGuangLong
- * @since 2024/5/31 14:23
-*/
+  /**
+   * 画出主要界面
+   * @author ChenGuangLong
+   * @since 2024/5/31 14:23
+   */
   drawOtherCom = () => {
     const { data } = this.state
     return (
@@ -83,7 +82,7 @@ export default class ReleaseSlipsPermissionsSearch extends QuickFormSearchPage {
                   background: '#fafafa',
                 }}
               >
-                <b>{index+1}、</b> [{item.RELEASECODE}]{item.RELEASENAME}
+                <b>{index + 1}、</b> [{item.RELEASECODE}]{item.RELEASENAME}
 
                 {/* 靠右的删除按钮 */}
                 <Popconfirm
@@ -105,7 +104,6 @@ export default class ReleaseSlipsPermissionsSearch extends QuickFormSearchPage {
         </Row>
 
 
-
         <CreatePageModal
           modal={{
             title: '新增放行人',
@@ -116,16 +114,16 @@ export default class ReleaseSlipsPermissionsSearch extends QuickFormSearchPage {
           onRef={node => (this.createPageModalRef = node)}
         />
       </>
-    )}
-
+    )
+  }
 
   /** ————————————————————————————————————————————————————————————————————————————————————————————————————————————
    * 删除一条数据
    * @param primaryKey 这条数据的主键
    * @author ChenGuangLong
    * @since 2024/5/31 14:21
-  */
-  delOne = primaryKey =>{
+   */
+  delOne = primaryKey => {
     dynamicDelete(
       {
         code: this.props.quickuuid,                       // quickuuid还是表名不知道，返回用一样的值就对了
